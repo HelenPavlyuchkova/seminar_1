2d56c24645fcdd9ebe5481d93ad32400c2c5722d
Helen <e.xolodkova@inbox.ru>
2022-04-10 12:48:51 +0300

    Решили 2 примера в С#

---
+++ primer_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Квадрат числа
+Console.WriteLine("Привет! Решим первую задачку?!");
+int numbera = 7;
+int numberb = 25;
+if(numbera * numbera == numberb)
+{
+   Console.WriteLine($"Число {numberb} является квадратом {numbera}");
+}
+else
+{
+    Console.WriteLine($"Число {numberb} не является квадратом {numbera}");
+}
