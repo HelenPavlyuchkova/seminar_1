8b55a22808b455cafdb4664be3b2678e0a8ac694
Helen <e.xolodkova@inbox.ru>
2022-04-10 13:30:30 +0300

    Добавили пример с днями недели

--- Primer_3/Program.cs
+++ Primer_3/Program.cs
@@ -1,5 +1,5 @@
 ﻿// дни недели
-int day = 5;
+int day = 1;
 if (day == 3)
 {
     Console.WriteLine("Cреда");
@@ -8,3 +8,7 @@ if (day == 5)
 {
     Console.WriteLine("Пятница");
 }
+if (day == 1)
+{
+    Console.WriteLine("Понедельник");
+    }
